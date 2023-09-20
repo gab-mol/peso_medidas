6ffdf66fb4bcadbc0d9201313ebcb189260271da
Gabriel Molina <gabrielmolina149@gmail.com>
2023-09-20 07:12:07 -0300

    init clase Archivo para guardar ruta

--- main.py
+++ main.py
@@ -10,36 +10,52 @@ Config.set('graphics', 'height', 300)
 import configparser
 
 class PesoApp(BoxLayout):
-	peso = ObjectProperty(None)
-	medsomx = ObjectProperty(None)
-	medsomn = ObjectProperty(None)
-	medbomx = ObjectProperty(None)
-	medbomn = ObjectProperty(None)
+    peso = ObjectProperty(None)
+    medsomx = ObjectProperty(None)
+    medsomn = ObjectProperty(None)
+    medbomx = ObjectProperty(None)
+    medbomn = ObjectProperty(None)
 
-	def guardar(self):
-		print(self.peso.text)
-		print(self.medsomx.text)
-		print(self.medsomn.text)
-		print(self.medbomx.text)
-		print(self.medbomn.text)
-		
-	def limpiar(self):
-		print("anda")
-	def mod_rut(self):
-		print("anda")
-	def abrir_xlsx(self):
-		print("anda")
+    def guardar(self):
+        print(self.peso.text)
+        print(self.medsomx.text)
+        print(self.medsomn.text)
+        print(self.medbomx.text)
+        print(self.medbomn.text)
+        
+    def limpiar(self):
+        print("anda")
+    def mod_rut(self):
+        print("anda")
+    def abrir_xlsx(self):
+        print("anda")
 
 class Archivo:
-	def __init__(self) -> None:
-		# para guardar ruta a archivo
-		config = configparser.ConfigParser()
+    '''Para guardar ruta a archivo.
+    (Quiero que sea editable por el usuario y persistente)'''
+    def __init__(self) -> None:
+        self.config = configparser.ConfigParser()
+        try:
+            self.config.read('segpeso.cfg')
+            self.ruta = self.config["RUTAS"]["xlsx"]
+        except:
+            self.config["RUTAS"] = {"xlsx": "0"}
+            with open('segpeso.cfg', 'w') as segpeso:
+                self.config.write(segpeso)
+            raise Exception("Sin archivo cfg, creado con valor nulo")
+        
+        print("Ruta a xlsx: ",self.ruta)
+
+    def crear_archivo():
+        ...
+    
 
 
 class MainApp(App):
-	title = "Seguimiento Peso"
-	def build(self):
-		return PesoApp()
-		
+    title = "Seguimiento Peso"
+    def build(self):
+        return PesoApp()
+        
 if __name__ == '__main__':
-	MainApp().run()
+    #MainApp().run()
+    r = Archivo()
