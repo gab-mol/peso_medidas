fb8aee3bd9a4cd59a7f2cb4399f64f09281bccbd
Gabriel Molina <gabrielmolina149@gmail.com>
2023-09-21 07:22:57 -0300

    conexión bd. Planteo de clase Crud.

--- main.py
+++ main.py
@@ -5,20 +5,29 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.config import Config
 # from kivy.uix.filechooser import FileChooser # por ahora prefiero evitarlo
+
 import os
 from plyer import filechooser
+
+from peewee import  SqliteDatabase, Model, DateField, FloatField
+
 Config.set('graphics', 'width', 500)
 Config.set('graphics', 'height', 300)
 
 import configparser
 
-class Archivos:
+RUTA = os.getcwd()
+
+
+
+# Bases de datos y archivo de configuración
+class Confg:
     '''Para guardar ruta a   Archivos.
     (Quiero que sea editable por el usuario y persistente)'''
     def __init__(self) -> None:
         '''Lee/Crea .cfg en dir trabajo.'''
         self.config = configparser.ConfigParser()
-        self.wdir = os.getcwd()
+        self.wdir = RUTA
         self.NOMBRE = 'segpeso.cfg'
         self.ruta_cfg = os.path.join(self.wdir, self.NOMBRE)
 
@@ -40,10 +49,56 @@ class Archivos:
         with open(self.ruta_cfg, 'w') as segpeso:
             self.config.write(segpeso)
 
+## Conexión con base de datos SQL (ORM)
+nombr_bd = "registro.db"
+bd = SqliteDatabase(nombr_bd)
+class Bd(Model):
+    class Meta():
+        database = bd
+
+class Registro(Bd):
+    '''Declaración de tabla'''
+    fecha = DateField()
+    peso = FloatField()
+    diametr_sob_ombl_mx = FloatField()
+    diametr_sob_ombl_mn = FloatField()
+    diametr_baj_ombl_mx = FloatField()
+    diametr_baj_ombl_mn = FloatField()
+
+try:
+    bd.connect()
+    bd.create_tables([Registro])
+    print(f"\n**\nÉxito al conectar con bd:\n {os.path.join(RUTA,nombr_bd)}\n**\n")
+except:
+    raise Exception("\nError de Conexión con bd SQL\n")
+
+class Crud():
+    '''Registrar medidas: al Excel que venía usando,
+    y a una base SQL (métodos CRUD).'''
+    def __init__(self, fecha:str, peso:float, medsomx:float,
+            medsomn:float, medbomx:float, medbomn:float) -> None:
+        
+        self.fecha = fecha
+        self.peso = peso
+        self.medsomx = medsomx
+        self.medsomn = medsomn
+        self.medbomx = medbomx
+        self.medbomn = medbomn
+    
+    def alta():
+        ...
+    
+    def baja():
+        ...
+        
+    def modificacion():
+        ...
+
+# Eventos bontones y declaración de app
 class PesoApp(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.segpeso_cfg = Archivos()
+        self.segpeso_cfg = Confg()
     
     peso = ObjectProperty(None)
     medsomx = ObjectProperty(None)
